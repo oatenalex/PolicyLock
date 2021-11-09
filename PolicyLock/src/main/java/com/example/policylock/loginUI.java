601500a043611ecc2c216f2e62ca18b5107445c3
scallait <sebastiencallait@gmail.com>
2021-11-09 14:40:50 -0800

    I hope this works

--- PolicyLock/src/main/java/com/example/policylock/loginUI.java
+++ PolicyLock/src/main/java/com/example/policylock/loginUI.java
@@ -7,12 +7,11 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class loginUI extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(loginUI.class.getResource("login.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setScene(scene);
         stage.show();
     }
