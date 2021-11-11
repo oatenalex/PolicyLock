4966a00ce2122c1f6aaf1aa1b22306bb0b580298
scallait <sebastiencallait@gmail.com>
2021-11-11 12:39:13 -0800

    Added Controller (Button Functionality to pages)

--- PolicyLock/src/main/java/com/example/policylock/settings.java
+++ PolicyLock/src/main/java/com/example/policylock/settings.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class settings extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(loginUI.class.getResource("settings.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(loginUI.class.getResource("devices.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
 
         stage.setTitle("PolicyLock");
